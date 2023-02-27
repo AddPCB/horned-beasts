185083ada9cc68906031bfec5f3fa347e412be1d
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-02-27 19:57:06 +0000

    First version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import { Header } from "./Header"
+import { Main } from "./Main"
+import { Footer } from "./Footer"
 function App() {
   return (
     <div className="App">
-      test
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
